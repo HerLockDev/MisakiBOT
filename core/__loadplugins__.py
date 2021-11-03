e379387c41378ed8ea239e7b88b7761589f22efe
@Herus31 <gidererenmustafa@gmail.com>
2021-11-03 21:53:07 +0300

    Update __loadplugins__.py

--- core/__loadplugins__.py
+++ core/__loadplugins__.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 def load_plugins():
-    for dosya in Client.iter_history(chat_id="me"):
+    for dosya in Client.iter_history(chat_id="self"):
         try:
             if dosya.document:
                 Client.download_media(file_name="./core/plugins")
