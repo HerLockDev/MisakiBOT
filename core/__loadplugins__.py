662c1147db6d7f2b097a2ffdd308ff2301017247
@Herus31 <gidererenmustafa@gmail.com>
2021-11-03 21:20:07 +0300

    Update __loadplugins__.py

--- core/__loadplugins__.py
+++ core/__loadplugins__.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-async def load_plugins(client:Client, message:Message):
-    for dosya in client.iter_history("me"):
+async def load_plugins():
+    for dosya in Client.iter_history("me"):
         try:
             if dosya.document:
-                await client.download_media(file_name="./core/plugins")
+                await Client.download_media(file_name="./core/plugins")
             else:
                 break
         except Exception as hata:
