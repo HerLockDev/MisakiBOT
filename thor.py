37f31f58a89ee96cf6e1f83aaaa4b411a34deae5
Herus31 <86302772+Herus31@users.noreply.github.com>
2021-11-04 21:15:06 +0300

    Update thor.py

--- thor.py
+++ thor.py
@@ -1,8 +1,12 @@
 from core import thor, baslangic
 from core.__loadplugins__ import load_plugins
-baslangic() # Bot'u Aç Kapa
-load_plugins()
+from pyrogram import idle
+async def run_bot():
+    baslangic()
+    await load_plugins()
+    await thor.start()
+    await idle()
+    await thor.stop()
 
-
-if __name__ == "__main__":
-    thor.run() 
+if name == '__main__':
+    thor.run(run_bot())
