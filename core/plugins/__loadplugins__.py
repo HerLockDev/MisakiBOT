cddb3cfbf938bf8b9e5a6040d6f80fa376748c92
@Herus31 <gidererenmustafa@gmail.com>
2021-11-05 20:47:43 +0300

    Update __loadplugins__.py

--- core/plugins/__loadplugins__.py
+++ core/plugins/__loadplugins__.py
@@ -1,23 +1,37 @@
+# PLUGIN_CHANNEL_ID kanalından pluginleri çekmeye yarayan modül ...
+# Herus
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from core import thor
-import importlib, traceback, asyncio
-
-@Client.on_message(filters.command(['loadplugs'], ['!','.','/']) & filters.me)
-async def load_plugins(client:Client,message:Message):
+from pyrogram.raw.functions.channels import CreateChannel
+import asyncio,importlib,os,sys
+from core import PLUGIN_CHANNEL_ID,thor
+@Client.on_message(filters.command(['loadp'], ['!','.','/']) & filters.me)
+async def loadp(client:Client, message:Message):
+    if PLUGIN_CHANNEL_ID is None:
+        channel = await client(CreateChannel(title = "Thor UserBot Plugin"))
+        channel["address"] = PLUGIN_CHANNEL_ID
     
-    async for dosya in thor.iter_history(chat_id="me"):
-        eklenti_dizini = f"./core/plugins/{dosya.file_name}"
-        
-        if dosya.document:
+    async for dosya in await thor.iter_history(chat_id=PLUGIN_CHANNEL_ID):
+        eklenti_dizini = f"./core/plugins/{dosya.document.file_name}"
+        if dosya.document and dosya.document.file_name.split(".")[-1] == "py":
             try:
-                await client.download_media(file_name=eklenti_dizini,message=dosya)
+                await client.download_media(message=dosya,file_name=eklenti_dizini)
                 await asyncio.sleep(2)
                 try:
                     spec = importlib.util.spec_from_file_location(eklenti_dizini, eklenti_dizini)
                     mod = importlib.util.module_from_spec(spec)
                     spec.loader.exec_module(mod)
+                except Exception as e:
+                    await client.send_message(chat_id = "me",message = "Plugin dosyalarından biri hatalı lütfen hatalı olanı düzelt veya sil.Hata: {e}")
+                    os.remove(eklenti_dizini)
+                    return
+                await client.send_message(chat_id = "me",message = "PLUGIN_CHANNEL_ID'deki tüm pluginler yüklendi ...")
+                try:
+                    await thor.stop()
                 except:
-                    traceback.print_exc()
-            except:
-                traceback.print_exc()
+                    pass
+                os.execl(sys.executable, sys.executable, *sys.argv)
+                return
+            except Exception as e:
+                print(str(e))
