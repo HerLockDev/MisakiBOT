e145a566051a460d6fbff57f3913f24ffdfad280
HerLockDev <94521956+HerLockDev@users.noreply.github.com>
2021-11-20 21:35:56 +0400

    Update _alive.py

--- core/plugins/_alive.py
+++ core/plugins/_alive.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from random import choice
     
-ALIVE_MESSAGES = ["Hocam Çalışıyorum...! MisakiUserBot"]
+ALIVE_MESSAGES = ["Hocam Çalışıyorum...! MisakiUserBot😎😎"]
 @Client.on_message(filters.command(['alive'], ['!','.','/']) & filters.me)
 async def komut(client:Client, message:Message):
 
